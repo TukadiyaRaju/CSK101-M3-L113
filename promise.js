94760f1dd0775e4a825350ce9c54e6987ee38c7a
TukadiyaRaju <115335924+TukadiyaRaju@users.noreply.github.com>
2022-12-12 23:08:49 +0530

    Initial commit

---
+++ promise.js
@@ -0,0 +1,36 @@
+const cookies = [{ name: 'Chocolate Cookies' }, { name: 'Macaron Cookies' }];
+const newCookie = { name: 'Biscotti Cookies' };
+
+// Progression 1: create a function to get all the cookies
+function getCookies() {
+  let name = [];
+  cookies.forEach((e) => {
+    name.push(e.name);
+  });
+
+  // Progression 2: using setTimeout() -- use 1000 units for time parameter
+  setTimeout(() => {
+    console.log(name);
+  }, 1000);
+}
+
+//Progression 3: Create a function to creat cookies and create an object of Promise.
+function createCookie() {
+  return new Promise((resolve, reject) => {
+    // Progression 4: adding two parameters and using setTimeout() -- use 2000 units for time parameter
+    setTimeout(() => {
+      // Progression 5: handling errors and adding new cookie to the list
+      cookies.push(newCookie);
+      let err = false;
+
+      if (!err) {
+        resolve();
+      } else {
+        reject('No cookie found');
+      }
+    }, 2000);
+  });
+}
+
+// Progression 6: call function using `.then`
+createCookie().then(getCookies());
